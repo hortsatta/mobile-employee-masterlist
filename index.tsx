c793b1a93a05c5f5ee8c1eb50d29020bdb67ee96
Erwin Errol Calanuga <hortsatta@gmail.com>
2021-10-19 21:02:29 +0800

    Invoke enableScreens

--- index.tsx
+++ index.tsx
@@ -1,4 +1,5 @@
 import 'react-native-gesture-handler';
+import { enableScreens } from 'react-native-screens';
 import React, { FC } from 'react';
 import { Provider as ReduxProvider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
@@ -8,6 +9,8 @@ import registerRootComponent from 'expo/build/launch/registerRootComponent';
 import { persistor, store } from 'store';
 import { HeaderContextProvider } from 'features/core/context';
 import { App } from 'features/core/components/app.component';
+
+enableScreens();
 
 const Root: FC = () => (
   <ReduxProvider store={store}>
