3676bbd04fb097867dd5c002644c45ec1a22fa15
Erwin Errol Calanuga <hortsatta@gmail.com>
2021-10-21 19:22:21 +0800

    Add job title by group selector

--- src/store/job-title/job-title.selectors.ts
+++ src/store/job-title/job-title.selectors.ts
@@ -1,4 +1,6 @@
 import { createSelector } from 'reselect';
+import * as d3 from 'd3-array';
+
 import { AppState } from '../app';
 import { jobTitleAdapter, JobTitleState } from './job-title.state';
 
@@ -16,6 +18,43 @@ const selectAllJobTitleEntities = createSelector(
   selectEntities
 );
 
+const selectAllJobTitlesGroupedByDepartment = createSelector(
+  selectAllJobTitles,
+  jobTitles => {
+    const groups = d3.groups(jobTitles, d => d.departmentId);
+    return groups.map(group => ({ departmentId: group[0], data: group[1] }));
+  }
+);
+
+const selectFilteredJobTitlesGroupedByDepartment = createSelector(
+  selectJobTitleState,
+  selectAllJobTitlesGroupedByDepartment,
+  (state, jobTitlesByDepartment) => {
+    const keyword = state.searchKeyword?.toLowerCase() || '';
+    let isEmpty = true;
+
+    const items = jobTitlesByDepartment
+      .filter(item => state.filterDepartment?.trim()
+        ? item.departmentId === state.filterDepartment
+        : true)
+      .map(item2 => {
+        const data = item2.data.filter(jt => jt.name.toLowerCase().includes(keyword));
+        isEmpty = !data.length;
+        return { ...item2, data };
+      });
+
+    return isEmpty ? [] : items;
+  }
+);
+
+const selectJobTitleFilters = createSelector(
+  selectJobTitleState,
+  state => ({
+    searchKeyword: state.searchKeyword,
+    filterDepartment: state.filterDepartment
+  })
+);
+
 const selectTotalJobTitleCount = createSelector(
   selectJobTitleState,
   selectTotal
@@ -29,6 +68,9 @@ const selectJobTitleLoading = createSelector(
 export {
   selectAllJobTitles,
   selectAllJobTitleEntities,
+  selectAllJobTitlesGroupedByDepartment,
+  selectFilteredJobTitlesGroupedByDepartment,
+  selectJobTitleFilters,
   selectTotalJobTitleCount,
   selectJobTitleLoading
 };
